e9e49177fd4bf427471bb18698ad05b522e6b301
kotetuco <paprapapra@gmail.com>
2017-03-05 17:15:06 +0900

    描画系初期化処理と画面を緑色に塗りつぶす処理を追加。

--- first.rs
+++ first.rs
@@ -1,17 +1,44 @@
 //
-// kotetuco, 2016
+// kotetuco, 2017
 // 
 
 #![feature(lang_items)]
 #![feature(start)]
-#![no_main]
 #![no_std]
 #![feature(asm)]
 
 #[no_mangle]
 pub extern "C" fn entry() {
-    loop {
+    init_graphic();
+
+    let color:u16 = convert_u16_color(0, 255, 0);
+    let vram_address:u32 = 0x06000000;
+
+    for y in 0..160 {
+        for x in 0..240 {
+            let offset: u32 = ((y * 240) + x) as u32;
+            let vram: *mut u16 = (vram_address + (offset * 2)) as *mut u16;
+            unsafe {
+                *vram = color;
+            }
+        }
     }
+
+    loop {}
+}
+
+fn init_graphic() {
+    let ioram_address: u32 = 0x04000000;
+    unsafe {
+        let video_mode: *mut u8 = ioram_address as *mut u8;
+        *video_mode = 0x03; // mode 3
+        let bg: *mut u8 = (ioram_address + 1) as *mut u8;
+        *bg = 0x04; // BG2
+    }
+}
+
+fn convert_u16_color(r:u8, g:u8, b:u8) -> u16{
+    return (((b >> 3) as u16) << 10) + (((g >> 3) as u16) << 5) + (r >> 3) as u16;
 }
 
 #[lang = "panic_fmt"]
@@ -19,6 +46,5 @@ extern fn panic_fmt() -> ! {
     loop {}
 }
 
-//#[no_mangle]
 #[lang = "eh_personality"]
 pub extern fn eh_personality() {}
